b9d50771a4d8d09c985262f54a7f4fc40b1755bc
reed17 <777reed777@gmail.com>
2018-06-19 02:35:15 +0300

    db.properties file created and filled with database parameters, created method getConnection() at DBUtils class

--- shop/src/main/java/com/ra/courses/project/utils/DBUtils.java
+++ shop/src/main/java/com/ra/courses/project/utils/DBUtils.java
@@ -1,6 +1,18 @@
 package com.ra.courses.project.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
 public class DBUtils {
+
+    private static final Logger LOGGER = Logger.getLogger(DBUtils.class);
 
     /**
      * Method contains a query for table creation.
@@ -24,4 +36,28 @@ public class DBUtils {
         return "DROP TABLE ORDERS IF EXISTS";
     }
 
+    /**
+     * Method creates connection to database using driver class, url, username and password,
+     * that stored in db.properties file.
+     * @return Connection.
+     */
+    public Connection getConnection() {
+        final Properties properties = new Properties();
+        Connection connection = null;
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(
+                    new File("/home/reed/IdeaProjects/neptune/shop/src/main/resources/db.properties"));
+            properties.load(fis);
+            Class.forName(properties.getProperty("DRIVER_CLASS"));
+            final String url = properties.getProperty("URL");
+            final String username = properties.getProperty("USERNAME");
+            final String password = properties.getProperty("PASSWORD");
+            connection = DriverManager.getConnection(url, username, password);
+            LOGGER.info("Connection successful!");
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return connection;
+    }
 }
