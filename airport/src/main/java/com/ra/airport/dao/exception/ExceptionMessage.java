6c344e1fdce675845e4536389923ee5339363bb9
Prorockkk <39459193+Prorockkk@users.noreply.github.com>
2018-07-14 21:41:14 +0300

    Update nep-3

--- airport/src/main/java/com/ra/airport/dao/exception/ExceptionMessage.java
+++ airport/src/main/java/com/ra/airport/dao/exception/ExceptionMessage.java
@@ -11,12 +11,11 @@ public enum ExceptionMessage {
     FAILED_TO_GET_FLIGHT_WITH_ID("Failed to get flight with id "),
     FAILED_TO_GET_ALL_FLIGHTS("Failed to get all flights"),
     FLIGHT_ID_CANNOT_BE_NULL("Flight id can't be null"),
-    FAILED_TO_CREATE_NEW_AIRPORT("Failed to create new flight"),
-    FAILED_TO_UPDATE_AIRPORT_WITH_ID("Failed to update flight with id "),
-    FAILED_TO_DELETE_AIRPORT_WITH_ID("Failed to delete flight with id "),
-    FAILED_TO_GET_AIRPORT_WITH_ID("Failed to get flight with id "),
-    FAILED_TO_GET_ALL_AIRPORTS("Failed to get all flights"),
-    AIRPORT_ID_CANNOT_BE_NULL("Flight id can't be null");
+    FAILED_TO_CREATE_NEW_AIRPORT("Failed to create new airport"),
+    FAILED_TO_UPDATE_AIRPORT_WITH_ID("Failed to update airport with id "),
+    FAILED_TO_DELETE_AIRPORT_WITH_ID("Failed to delete airport with id "),
+    FAILED_TO_GET_AIRPORT_WITH_ID("Failed to get airport with id "),
+    FAILED_TO_GET_ALL_AIRPORTS("Failed to get all airports");
 
     private final String message;
 
