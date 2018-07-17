c9d3c70235df6c72e07d582ef22e3fac9d68003b
Anton Bondar <AntonBondar2013@gmail.com>
2018-07-17 08:22:17 +0300

    NEP-18 Migrate Flight entity to Spring Correct config class.

--- airport/src/main/java/com/ra/airport/configuration/AirPortConfiguration.java
+++ airport/src/main/java/com/ra/airport/configuration/AirPortConfiguration.java
@@ -2,28 +2,33 @@ package com.ra.airport.configuration;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
+@PropertySource("classpath:config.properties")
 @Configuration
 public class AirPortConfiguration {
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DataSource dataSource () {
-        return new HikariDataSource();
+        return new HikariDataSource(dataSourceConfig());
     }
 
     @Bean
     public HikariConfig dataSourceConfig() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl( "jdbc_url" );
-        config.setUsername( "database_username" );
-        config.setPassword( "database_password" );
-        config.addDataSourceProperty( "cachePrepStmts" , "true" );
-        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
-        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
-        ds = new HikariDataSource( config );
+        HikariConfig config = new HikariConfig(environment.getProperty());
+
+        config.setJdbcUrl(environment.getProperty("jdbc.url"));
+        config.setUsername(environment.getProperty("jdbc.user"));
+        config.setPassword(environment.getProperty("jdbc.password"));
+        return config;
     }
 }
