d307f342843454b4d552941d6b4279ba3cd3c9ea
velykyi.petro <velykyi.petro@gmail.com>
2018-07-17 19:00:47 +0300

    Deleted TicketDaoExeption

--- airport/src/main/java/com/ra/airport/dao/exception/TicketDaoException.java
+++
@@ -1,7 +0,0 @@
-package com.ra.airport.dao.exception;
-
-public class TicketDaoException extends Exception {
-    public TicketDaoException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
