548cb6e2322944628218c127d3939e23823d2d45
velykyi.petro <velykyi.petro@gmail.com>
2018-07-28 01:37:11 +0300

    Rewrote TicketDao for Spring JDBC + Tests

--- airport/src/main/java/com/ra/airport/dao/impl/TicketDao.java
+++ airport/src/main/java/com/ra/airport/dao/impl/TicketDao.java
@@ -1,7 +1,5 @@
 package com.ra.airport.dao.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
@@ -14,7 +12,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
