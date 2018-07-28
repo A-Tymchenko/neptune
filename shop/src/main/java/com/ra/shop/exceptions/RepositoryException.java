62c0e66d7d52b9f06757855c2b20bb743e853a79
reed17 <777reed777@gmail.com>
2018-07-28 10:28:34 +0300

    exceptions added

--- shop/src/main/java/com/ra/shop/exceptions/RepositoryException.java
+++ shop/src/main/java/com/ra/shop/exceptions/RepositoryException.java
@@ -1,4 +1,26 @@
 package com.ra.shop.exceptions;
 
-public class RepositoryException {
+/**
+ * Exception can be thrown if CRUD operations can`t be performed.
+ */
+public class RepositoryException extends Exception {
+
+    /**
+     * Creates RepositoryException instance, accepts exception message itself.
+     *
+     * @param message error message.
+     */
+    public RepositoryException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Creates RepositoryException instance, accepts exception message and the exception itself.
+     *
+     * @param message error message.
+     * @param cause   exception itself.
+     */
+    public RepositoryException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
