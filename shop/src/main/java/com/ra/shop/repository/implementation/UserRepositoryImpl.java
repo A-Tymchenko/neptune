63f26d21549d927b0ed78f7cdfb9ebf6a76c0370
Anton <towu4.100@gmail.com>
2018-08-05 19:46:22 +0300

    Migrate entity user to spring

---
+++ shop/src/main/java/com/ra/shop/repository/implementation/UserRepositoryImpl.java
@@ -0,0 +1,213 @@
+package com.ra.shop.repository.implementation;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import com.ra.shop.exceptions.RepositoryException;
+import com.ra.shop.model.User;
+import com.ra.shop.repository.IRepository;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+/**
+ * IRepository implementation using Spring jdbc.
+ */
+@Component
+public class UserRepositoryImpl implements IRepository<User> {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(UserRepositoryImpl.class);
+
+    /**
+     * KeyHolder instance stores primary keys.
+     */
+    private final transient KeyHolder keyHolder = new GeneratedKeyHolder();
+
+    /**
+     * JdbcTemplate instance.
+     */
+    private final transient JdbcTemplate jdbcTemplate;
+
+    /**
+     * Constructor accepts jdbcTemplate as a parameter.
+     *
+     * @param jdbcTemplate jdbcTemplate
+     */
+    @Autowired
+    public UserRepositoryImpl(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /**
+     * Method adds an entity to database.
+     *
+     * @param entity that will be created.
+     * @return User persisted entity.
+     * @throws RepositoryException can be thrown if any error occurs.
+     */
+    @Override
+    public User create(final User entity) throws RepositoryException {
+        Objects.requireNonNull(entity);
+        final Long userId;
+        try {
+            jdbcTemplate.update(con -> {
+                final PreparedStatement statement = con.prepareStatement(
+                        "INSERT INTO USERS (PHONE_NUMBER, NAME, SECOND_NAME, COUNTRY, EMAIL_ADDRESS) "
+                                + "VALUES(?, ?, ?, ?, ?)");
+                fillEntityWithParameters(statement, entity);
+                return statement;
+            }, keyHolder);
+            userId = (Long) keyHolder.getKey();
+        } catch (DataAccessException e) {
+            final String message = String.format("User{id:%d} NOT CREATED!", entity.getId());
+            LOGGER.error(message, e);
+            throw new RepositoryException(message, e);
+        }
+        entity.setId(userId);
+        return entity;
+    }
+
+    /**
+     * Method performs search in database and returns an entity with pointed id.
+     *
+     * @param entityId - id of searched entity.
+     * @return User searched entity.
+     * @throws RepositoryException can be thrown if any error occurs.
+     */
+    @Override
+    public User get(final Long entityId) throws RepositoryException {
+        final User found;
+        try {
+            final BeanPropertyRowMapper<User> mapper = BeanPropertyRowMapper.newInstance(User.class);
+            found = jdbcTemplate.queryForObject(
+                    "SELECT * FROM USERS WHERE USER_ID = ?",
+                    mapper,
+                    entityId);
+        } catch (DataAccessException e) {
+            final String message = String.format("User{id:%d} NOT FOUND!", entityId);
+            LOGGER.error(message, e);
+            throw new RepositoryException(message, e);
+        }
+        return found;
+    }
+
+    /**
+     * Method performs update operation.
+     *
+     * @param newEntity updated version of entity.
+     * @return User updated version of entity.
+     * @throws RepositoryException can be thrown if any error occurs.
+     */
+    @Override
+    public User update(final User newEntity) throws RepositoryException {
+        Objects.requireNonNull(newEntity);
+        try {
+            jdbcTemplate.update(
+                    "UPDATE USERS SET PHONE_NUMBER= ?,NAME = ?,SECOND_NAME= ?,COUNTRY= ?,EMAIL_ADDRESS= ? WHERE USER_ID= ?",
+                    statement -> {
+                        fillEntityWithParameters(statement, newEntity);
+                        final int userId = 6;
+                        statement.setLong(userId, newEntity.getId());
+                    });
+            return newEntity;
+        } catch (DataAccessException e) {
+            final String message = String.format("User{id:%d} NOT UPDATED!", newEntity.getId());
+            LOGGER.error(message, e);
+            throw new RepositoryException(message, e);
+        }
+    }
+
+    /**
+     * Method performs delete operation.
+     *
+     * @param entityId of entity that will be deleted.
+     * @return boolean true if entity removed successfully or false if not.
+     * @throws RepositoryException can be thrown if any error occurs.
+     */
+    @Override
+    public boolean delete(final Long entityId) throws RepositoryException {
+        try {
+            final int deletedRowsNumber = jdbcTemplate.update(
+                    "DELETE FROM USERS WHERE USER_ID = ?",
+                    entityId);
+            return deletedRowsNumber > 0;
+        } catch (DataAccessException e) {
+            final String message = String.format("User{id:%d} NOT DELETED!", entityId);
+            LOGGER.error(message, e);
+            throw new RepositoryException(message, e);
+        }
+    }
+
+    /**
+     * Method returns list of entities that stored in database.
+     *
+     * @return List list of existed users.
+     * @throws RepositoryException can be thrown if any error occurs.
+     */
+    @Override
+    public List<User> getAll() throws RepositoryException {
+        final List<Map<String, Object>> map;
+        try {
+            map = jdbcTemplate.queryForList("SELECT * FROM USERS");
+            getListOfUsers(map);
+        } catch (DataAccessException e) {
+            final String message = "No users found in database!";
+            LOGGER.error(message, e);
+            throw new RepositoryException(message, e);
+        }
+        return getListOfUsers(map);
+    }
+
+    /**
+     * Method iterate over the list of users, set params into user instances and adds them to ArrayList and returns.
+     *
+     * @param map stores params for each entity.
+     * @return List list of users.
+     */
+    public List<User> getListOfUsers(final List<Map<String, Object>> map) {
+        final List<User> users = new ArrayList<>();
+        final User user = new User();
+        for (final Map<String, Object> row : map) {
+            user.setId((Long) row.get("USER_ID"));
+            user.setPhoneNumber((String) row.get("PHONE_NUMBER"));
+            user.setName((String) row.get("NAME"));
+            user.setSecondName((String) row.get("SECOND_NAME"));
+            user.setCountry((String) row.get("COUNTRY"));
+            user.setEmailAddress((String) row.get("EMAIL_ADDRESS"));
+            users.add(user);
+        }
+        return users;
+    }
+
+    /**
+     * Method fills user entity with parameters using preparedStatement.
+     *
+     * @param statement PreparedStatement.
+     * @param user     user entity that will be created.
+     * @throws SQLException if any error occurs.
+     */
+    private void fillEntityWithParameters(final PreparedStatement statement, final User user) throws SQLException {
+        final int phoneNumber = 1;
+        final int name = 2;
+        final int secondName = 3;
+        final int country = 4;
+        final int emailAddress = 5;
+        statement.setString(phoneNumber, user.getPhoneNumber());
+        statement.setString(name, user.getName());
+        statement.setString(secondName, user.getSecondName());
+        statement.setString(country, user.getCountry());
+        statement.setString(emailAddress, user.getEmailAddress());
+    }
+}
---
+++ shop/src/test/java/com/ra/shop/repository/implementation/UserRepositoryMockTest.java
@@ -0,0 +1,207 @@
+package com.ra.shop.repository.implementation;
+
+import com.ra.shop.exceptions.RepositoryException;
+import com.ra.shop.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class UserRepositoryMockTest {
+
+    private static UserRepositoryImpl repository;
+    private static JdbcTemplate jdbcTemplate;
+    private KeyHolder keyHolder;
+
+    private Connection connection;
+    private PreparedStatement statement;
+
+    @BeforeAll
+    static void initGlobal() {
+        jdbcTemplate = mock(JdbcTemplate.class);
+        repository = new UserRepositoryImpl(jdbcTemplate);
+    }
+
+    @BeforeEach
+    void setup() {
+        keyHolder = mock(KeyHolder.class);
+        connection = mock(Connection.class);
+        statement = mock(PreparedStatement.class);
+    }
+
+    @Test
+    void whenCreateUserThenReturnCreatedUser() throws RepositoryException, SQLException {
+        User user = new User( "3809934252275", "Pasha", "Volum",
+                "Moscow", "pasha_213@gmail.com");
+        when(connection.prepareStatement(
+                eq("INSERT INTO USERS (PHONE_NUMBER, NAME, SECOND_NAME, COUNTRY, EMAIL_ADDRESS) "
+                        + "VALUES(?, ?, ?, ?, ?)"))).thenReturn(statement);
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((PreparedStatementCreator) invocation.getArguments()[0]).createPreparedStatement(connection);
+                return null;
+            }
+        }).when(jdbcTemplate).update(any(PreparedStatementCreator.class), any(KeyHolder.class));
+        when(keyHolder.getKey()).thenReturn(1L);
+        User created = repository.create(user);
+        user.setId((long) keyHolder.getKey());
+        assertNotNull(created);
+    }
+
+    @Test
+    void whenGetUserThenReturnCorrectEntity() throws RepositoryException {
+        User user = new User( "3806642341542", "Murchik", "Babulin",
+                "USA", "murchik_21@gmail.com");
+        user.setId(1L);
+        when(jdbcTemplate
+                .queryForObject(
+                        eq("SELECT * FROM USERS WHERE USER_ID = ?"),
+                        any(RowMapper.class),
+                        any(Object[].class))).thenReturn(user);
+        User found = repository.get(user.getId());
+        assertNotNull(found);
+        assertEquals(user, found);
+    }
+
+    @Test
+    void whenDeleteUserThenReturnSuccessfulQueryExecutionResultTrue() throws RepositoryException {
+        User user = new User( "3806642341542", "Murik", "Balin",
+                "USA", "urchik_457@gmail.com");
+        user.setId(5L);
+        when(jdbcTemplate.update(eq("DELETE FROM USERS WHERE USER_ID = ?"), any(Object.class))).thenReturn(1);
+        boolean isDeleted = repository.delete(user.getId());
+        assertTrue(isDeleted);
+    }
+
+    @Test
+    void whenDeleteUserThenReturnUnsuccessfulQueryExecutionResultFalse() throws RepositoryException {
+        User user = new User( "3809765435266", "Taras ", "Shevchenko",
+                "Ukraine", "taras_13@gmail.com");
+        user.setId(6L);
+        when(jdbcTemplate.update(eq("DELETE FROM USERS WHERE USER_ID = ?"), any(Object.class))).thenReturn(0);
+        boolean isDeleted = repository.delete(user.getId());
+        assertFalse(isDeleted);
+    }
+
+    @Test
+    void whenGetAllThenReturnListOfExistedUsers() throws RepositoryException {
+        List<Map<String, Object>> users = getListOfUsers();
+        when(jdbcTemplate.queryForList(eq("SELECT * FROM USERS"))).thenReturn(users);
+        List<User> actual = repository.getAll();
+        List<User> expected = repository.getListOfUsers(users);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenUpdateUserThenReturnUpdatedUser() throws RepositoryException {
+        User user = new User("3809934252275", "Pasha", "Volum",
+                "Moscow", "pasha_213@gmail.com");
+        user.setId(1L);
+        final String query = "UPDATE USERS SET PHONE_NUMBER= ?,NAME = ?,SECOND_NAME= ?," +
+                "COUNTRY= ?,EMAIL_ADDRESS= ? WHERE USER_ID= ?";
+        doAnswer(invocation -> {
+            ((PreparedStatementSetter) invocation.getArguments()[1]).setValues(statement);
+            return null;
+        }).when(jdbcTemplate).update(eq(query), any(PreparedStatementSetter.class));
+        User updated = repository.update(user);
+        assertNotNull(updated);
+        assertEquals(user, updated);
+    }
+
+    @Test
+    void whenGetUserThenThrowRepositoryException() {
+        when(jdbcTemplate
+                .queryForObject(
+                        eq("SELECT * FROM USERS WHERE USER_ID = ?"),
+                        any(RowMapper.class),
+                        any(Object[].class)))
+                .thenThrow(new DataAccessException(""){});
+        Throwable repositoryException = assertThrows(RepositoryException.class, () -> {
+            repository.get(new User().getId());
+        });
+        assertNotNull(repositoryException);
+        assertEquals(RepositoryException.class, repositoryException.getClass());
+    }
+
+    @Test
+    void whenUpdateUserThenThrowRepositoryException() {
+        when(jdbcTemplate.update(anyString(), any(PreparedStatementSetter.class))).thenThrow(new DataAccessException(""){});
+        Throwable repositoryException = assertThrows(RepositoryException.class, () -> {
+            repository.update(new User());
+        });
+        assertNotNull(repositoryException);
+        assertEquals(RepositoryException.class, repositoryException.getClass());
+    }
+
+    @Test
+    void whenCreateUserThenThrowRepositoryException() throws SQLException {
+        User user = new User("3806754352134", "Taras", "Mazur",
+                "Ukraine", "mazur_123@gmail.com");
+        when(connection.prepareStatement(
+                eq("INSERT INTO USERS (PHONE_NUMBER, NAME, SECOND_NAME, COUNTRY, EMAIL_ADDRESS) "
+                        + "VALUES(?, ?, ?, ?, ?)"))).thenReturn(statement);
+        doThrow(new DataAccessException(""){})
+                .when(jdbcTemplate).update(any(PreparedStatementCreator.class), any(KeyHolder.class));
+        Throwable repositoryException = assertThrows(RepositoryException.class, () -> {
+            repository.create(user);
+        });
+        assertNotNull(repositoryException);
+        assertEquals(RepositoryException.class, repositoryException.getClass());
+    }
+
+    @Test
+    void whenGetAllUsersThenThrowRepositoryException() {
+        when(jdbcTemplate.queryForList(eq("SELECT * FROM USERS"))).thenThrow(new DataAccessException(""){});
+        Throwable repositoryException = assertThrows(RepositoryException.class, () -> {
+            repository.getAll();
+        });
+        assertEquals(RepositoryException.class, repositoryException.getClass());
+    }
+
+    @Test
+    void whenDeleteUserThenThrowRepositoryException() {
+        when(jdbcTemplate.update(
+                eq("DELETE FROM USERS WHERE USER_ID = ?"),
+                any(Object.class))).thenThrow(new DataAccessException(""){});
+        Throwable repositoryException = assertThrows(RepositoryException.class, () -> {
+            repository.delete(new User().getId());
+        });
+        assertNotNull(repositoryException);
+        assertEquals(RepositoryException.class, repositoryException.getClass());
+    }
+
+    private List<Map<String, Object>> getListOfUsers() {
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        User user = new User( "3806734536743", "Adolf", "Hitlerl",
+                "German", "adolfyk_1945@gmail.com");
+        user.setId(9L);
+        map.put("USER_ID", user.getId());
+        map.put("PHONE_NUMBER", user.getPhoneNumber());
+        map.put("NAME", user.getName());
+        map.put("SECOND_NAME", user.getSecondName());
+        map.put("COUNTRY", user.getCountry());
+        map.put("EMAIL_ADDRESS", user.getEmailAddress());
+        mapList.add(map);
+        return mapList;
+    }
+
+}
