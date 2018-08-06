931f2fce2156b01bd5aebddc4e6225ad0c4d1c2e
Roman Andriianov <randriyanov9@gmail.com>
2018-08-06 13:14:56 +0300

    small refactoring GoodsRepositoryMockTest

--- shop/src/test/java/com/ra/shop/repository/implementation/GoodsRepositoryMockTest.java
+++ shop/src/test/java/com/ra/shop/repository/implementation/GoodsRepositoryMockTest.java
@@ -1,12 +1,10 @@
 package com.ra.shop.repository.implementation;
 
 import com.ra.shop.exceptions.RepositoryException;
-import com.ra.shop.repository.implementation.GoodsRepositoryImpl;
 import com.ra.shop.model.Goods;
 import org.junit.jupiter.api.*;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.*;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import java.sql.*;
@@ -17,148 +15,134 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
-public class GoodsRepositoryMockTest {
+class GoodsRepositoryMockTest {
 
     private static GoodsRepositoryImpl dao;
-    private static JdbcTemplate mockedjdbcTemplate;
-    private static Goods existingGoods = new Goods("Camel", 7622210609779l, 1.2d);
-    private Connection mockedConnection;
-    private PreparedStatement mockedStatement;
-    private ResultSet mockedResultSet;
-    private ResultSet mockedResultSetForKey;
-    private KeyHolder mockedgeneratedKeys = new GeneratedKeyHolder();
-    private BeanPropertyRowMapper mockedBeanPropertyRowMapper;
-    private static final Long DEFAULT_ID = 1l;
-    private static final Long GENERATION_ID = 2l;
+    private static JdbcTemplate mockJdbcTemplate;
+    private static Goods TEST_GOODS = new Goods("Camel", 7622210609779L, 1.2d);
+    private Connection mockConnection;
+    private PreparedStatement mockStatement;
+    private KeyHolder mockGeneratedKeyHolder;
+    private static final Long DEFAULT_ID = 1L;
+    private static final Long GENERATION_ID = 2L;
 
     @BeforeAll
     static void createSchema() {
-        mockedjdbcTemplate = mock(JdbcTemplate.class);
-        dao = new GoodsRepositoryImpl(mockedjdbcTemplate);
-        existingGoods.setId(DEFAULT_ID);
+        TEST_GOODS.setId(DEFAULT_ID);
     }
 
     @BeforeEach
-    public void setUp() {
-        mockedgeneratedKeys = mock(KeyHolder.class);
-        mockedBeanPropertyRowMapper = mock(BeanPropertyRowMapper.class);
-        mockedConnection = mock(Connection.class);
-        mockedStatement = mock(PreparedStatement.class);
-        mockedResultSet = mock(ResultSet.class);
-        mockedResultSetForKey = mock(ResultSet.class);
+    void setUp() {
+        mockJdbcTemplate = mock(JdbcTemplate.class);
+        dao = new GoodsRepositoryImpl(mockJdbcTemplate);
+        mockGeneratedKeyHolder = mock(KeyHolder.class);
+        mockConnection = mock(Connection.class);
+        mockStatement = mock(PreparedStatement.class);
+        when(mockGeneratedKeyHolder.getKey()).thenReturn(GENERATION_ID);
     }
 
     @Test
     void whenCreatedGoodsWithFalseNextIdThenReturnEqualsGoods() throws RepositoryException, SQLException {
-        when(mockedConnection.prepareStatement(eq("INSERT INTO GOODS (NAME, BARCODE, PRICE) VALUES (?,?,?)")))
-                .thenReturn(mockedStatement);
+        when(mockConnection.prepareStatement("INSERT INTO GOODS (NAME, BARCODE, PRICE) VALUES (?,?,?)"))
+                .thenReturn(mockStatement);
         doAnswer(invocation -> {
-            ((PreparedStatementCreator) invocation.getArguments()[0]).createPreparedStatement(mockedConnection);
+            ((PreparedStatementCreator) invocation.getArguments()[0]).createPreparedStatement(mockConnection);
             return null;
-        }).when(mockedjdbcTemplate).update(any(PreparedStatementCreator.class), any(KeyHolder.class));
-        when(mockedgeneratedKeys.getKey()).thenReturn(GENERATION_ID);
-        Goods goods = dao.create(existingGoods);
-        existingGoods.setId((Long) mockedgeneratedKeys.getKey());
-        assertTrue(goods.equals(existingGoods));
+        }).when(mockJdbcTemplate).update(any(PreparedStatementCreator.class), any(KeyHolder.class));
+
+        Goods resultGoods = dao.create(TEST_GOODS);
+        TEST_GOODS.setId((Long) mockGeneratedKeyHolder.getKey());
+
+        assertEquals(TEST_GOODS, resultGoods);
     }
 
     @Test
-    void whenReadAllGoodThenResultIsTrue() throws Exception {
-        when(mockedjdbcTemplate.queryForList(eq("SELECT * FROM GOODS"))).thenReturn(createListOfMap());
+    void whenReadAllGoodThenReturnsNotEmptyList() throws Exception {
+        when(mockJdbcTemplate.queryForList("SELECT * FROM GOODS")).thenReturn(createListOfMap());
+
         assertFalse(dao.getAll().isEmpty());
     }
 
     @Test
-    void whenDeletionGoodsThenReturnTrue() throws RepositoryException {
-        when(mockedjdbcTemplate.update(eq("DELETE FROM GOODS WHERE ID = ?"), eq(existingGoods.getId()))).thenReturn(1);
-        assertTrue(dao.delete(existingGoods.getId()));
+    void whenDeleteGoodsThenReturnTrue() throws RepositoryException {
+        when(mockJdbcTemplate.update("DELETE FROM GOODS WHERE ID = ?", TEST_GOODS.getId())).thenReturn(1);
+
+        assertTrue(dao.delete(TEST_GOODS.getId()));
     }
 
     @Test
-    void whenDeletionGoodsThenReturnFalse() throws RepositoryException {
-        when(mockedjdbcTemplate.update(eq("DELETE FROM GOODS WHERE ID = ?"),
-                eq(existingGoods.getId()))).thenReturn(-1);
-        assertFalse(dao.delete(existingGoods.getId()));
+    void whenGoodsCannotBeDeletedThenReturnFalse() throws RepositoryException {
+        when(mockJdbcTemplate.update("DELETE FROM GOODS WHERE ID = ?", TEST_GOODS.getId())).thenReturn(-1);
+
+        assertFalse(dao.delete(TEST_GOODS.getId()));
     }
 
     @Test
-    void getAllGoodsFailsWithExceptionAsFeedbackToClient() {
-        assertThrows(RepositoryException.class, () -> {
-            when(mockedjdbcTemplate.queryForList(eq("SELECT * FROM GOODS")))
-                    .thenThrow(EmptyResultDataAccessException.class);
-            dao.getAll();
-        });
+    void whenGetAllGoodsThrowsExceptionThenRepositoryExceptionThrown() {
+        when(mockJdbcTemplate.queryForList(anyString())).thenThrow(new EmptyResultDataAccessException(0));
+
+        assertThrows(RepositoryException.class, () -> dao.getAll());
     }
 
     @Test
     void whenGetGoodsWithTrueNextGoodsThenReturnEqualsGoods() throws RepositoryException {
-        when(mockedjdbcTemplate.queryForObject(eq("SELECT * FROM GOODS WHERE ID = ?"),
-                any(RowMapper.class),
-                eq(existingGoods.getId()))).thenReturn(existingGoods);
-        assertTrue(dao.get(existingGoods.getId()).equals(existingGoods));
+        when(mockJdbcTemplate.queryForObject(eq("SELECT * FROM GOODS WHERE ID = ?"), any(RowMapper.class),
+                eq(TEST_GOODS.getId()))).thenReturn(TEST_GOODS);
+
+        assertEquals(dao.get(TEST_GOODS.getId()), TEST_GOODS);
     }
 
     @Test
     void getAGoodByIdFailsWithExceptionAsFeedbackToClient() {
-        assertThrows(RepositoryException.class, () -> {
-            when(mockedjdbcTemplate.queryForObject(eq("SELECT * FROM GOODS WHERE ID = ?"),
-                    any(RowMapper.class), any(Long.class)))
-                    .thenThrow(EmptyResultDataAccessException.class);
-            dao.get(existingGoods.getId());
-        });
+        when(mockJdbcTemplate.queryForObject(anyString(), any(RowMapper.class), any(Long.class)))
+                .thenThrow(new EmptyResultDataAccessException(0));
+
+        assertThrows(RepositoryException.class, () -> dao.get(TEST_GOODS.getId()));
     }
 
     @Test
     void deletingAGoodsFailsWithExceptionAsFeedbackToTheClient() {
-        assertThrows(RepositoryException.class, () -> {
-            when(mockedjdbcTemplate.update(eq("DELETE FROM GOODS WHERE ID = ?"),
-                    eq(existingGoods.getId())))
-                    .thenThrow(EmptyResultDataAccessException.class);
-            dao.delete(existingGoods.getId());
-        });
+        when(mockJdbcTemplate.update(anyString(), anyInt())).thenThrow(new EmptyResultDataAccessException(0));
+
+        assertThrows(RepositoryException.class, () -> dao.delete(TEST_GOODS.getId()));
     }
 
     @Test
     void updatingAGoodsFailsWithFeedbackToTheClient() {
-        assertThrows(RepositoryException.class, () -> {
-            when(mockedjdbcTemplate.update(eq("UPDATE GOODS SET NAME = ?, BARCODE = ?, PRICE = ? WHERE ID = ?"),
-                    any(PreparedStatementSetter.class)))
-                    .thenThrow(EmptyResultDataAccessException.class);
-            dao.update(existingGoods);
-        });
+        when(mockJdbcTemplate.update(anyString(), any(PreparedStatementSetter.class)))
+                .thenThrow(new EmptyResultDataAccessException(0));
+
+        assertThrows(RepositoryException.class, () -> dao.update(TEST_GOODS));
     }
 
     @Test
-    public void createAGoodsFailsWithExceptionAsFeedbackToClient() {
+    void createAGoodsFailsWithExceptionAsFeedbackToClient() {
+        when(mockJdbcTemplate.update(any(PreparedStatementCreator.class), any(KeyHolder.class)))
+                .thenThrow(new EmptyResultDataAccessException(0));
 
-        assertThrows(RepositoryException.class, () -> {
-            when(mockedjdbcTemplate.update(any(PreparedStatementCreator.class),
-                    any(KeyHolder.class)))
-                    .thenThrow(EmptyResultDataAccessException.class);
-            dao.create(existingGoods);
-        });
+        assertThrows(RepositoryException.class, () -> dao.create(TEST_GOODS));
     }
 
     @Test
-    void whenUpdationGoodsThenReturnEqualsGoods() throws RepositoryException {
+    void whenUpdateGoodsThenReturnEqualsGoods() throws RepositoryException {
         doAnswer(invocation -> {
-            ((PreparedStatementSetter) invocation.getArguments()[1])
-                    .setValues(mockedStatement);
+            ((PreparedStatementSetter) invocation.getArguments()[1]).setValues(mockStatement);
             return null;
-        }).when(mockedjdbcTemplate)
+        }).when(mockJdbcTemplate)
                 .update(eq("UPDATE GOODS SET NAME = ?, BARCODE = ?, PRICE = ? WHERE ID = ?"),
                         any(PreparedStatementSetter.class));
-        Goods goods = dao.update(existingGoods);
-        assertTrue(goods.equals(existingGoods));
+        Goods goods = dao.update(TEST_GOODS);
+
+        assertEquals(goods, TEST_GOODS);
     }
 
     private List createListOfMap() {
         List<Map<String, Object>> listToMapFrom = new ArrayList<>();
         Map<String, Object> goods = new HashMap<>();
-        goods.put("ID", existingGoods.getId());
-        goods.put("NAME", existingGoods.getName());
-        goods.put("BARCODE", existingGoods.getBarcode());
-        goods.put("PRICE", existingGoods.getPrice());
+        goods.put("ID", TEST_GOODS.getId());
+        goods.put("NAME", TEST_GOODS.getName());
+        goods.put("BARCODE", TEST_GOODS.getBarcode());
+        goods.put("PRICE", TEST_GOODS.getPrice());
         listToMapFrom.add(goods);
         return listToMapFrom;
     }
