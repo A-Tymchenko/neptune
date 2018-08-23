150135d78eb463e3e1192dda83cd5c7e972acf8e
Anton_Bondar <AntonBondar2013@gmail.com>
2018-08-23 10:59:22 +0300

    Correct javadoc

--- airport/src/main/java/com/ra/airport/controller/MainController.java
+++ airport/src/main/java/com/ra/airport/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     /**
-     * Register bean for DataSourceInitializer.
+     * Return path to index.jsp.
      * @return index
      */
     @RequestMapping("/")
