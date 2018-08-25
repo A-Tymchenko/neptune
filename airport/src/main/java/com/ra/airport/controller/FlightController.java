9c3b61fd97a9eb9c71142bce2f2a587a8e4a0ef1
Anton Bondar <AntonBondar2013@gmail.com>
2018-08-25 14:02:56 +0300

    NEP-46-create-controller-for-flight Controller refactoring.

--- airport/src/main/java/com/ra/airport/controller/FlightController.java
+++ airport/src/main/java/com/ra/airport/controller/FlightController.java
@@ -13,8 +13,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,10 +26,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Controller for CRUD operations with {@link com.ra.airport.entity.Flight} entity.
  */
+
 @Controller
+@RequestMapping(value = "/flights")
 public class FlightController {
 
-    private static final String REQUEST_PATH = "/flights";
     private FlightService flightService;
 
     @Autowired
@@ -33,33 +38,32 @@ public class FlightController {
         this.flightService = flightService;
     }
 
+    @GetMapping
+    public String getFlights(final Model model) throws AirPortDaoException {
+        List<Flight> flights = flightService.getAll();
+        model.addAttribute("flights",flights);
 
-    @RequestMapping(value = REQUEST_PATH, method = RequestMethod.POST)
+        return "flight/flights";
+    }
+
+    @PostMapping
     public ResponseEntity<Flight> createFlights(final @Valid @RequestBody FlightDto flightDto) throws AirPortDaoException {
         final var flight = new Flight();
         BeanUtils.copyProperties(flightDto, flight);
         return new ResponseEntity<>(flightService.create(flight), HttpStatus.OK);
     }
 
-    @RequestMapping(value = REQUEST_PATH, method = RequestMethod.PUT)
+    @PutMapping
     public ResponseEntity<Flight> updateFlight(final @Valid @RequestBody FlightDto flightDto) throws AirPortDaoException {
         final var flight = new Flight();
         BeanUtils.copyProperties(flightDto, flight);
         return new ResponseEntity<>(flightService.update(flight), HttpStatus.OK);
     }
 
-    @RequestMapping(value = REQUEST_PATH, method = RequestMethod.DELETE)
+    @DeleteMapping
     public ResponseEntity<Boolean> deleteFlight(final @Valid @RequestBody FlightDto flightDto) throws AirPortDaoException {
         final var flight = new Flight();
         BeanUtils.copyProperties(flightDto, flight);
         return new ResponseEntity<>(flightService.delete(flight), HttpStatus.OK);
-    }
-    
-    @RequestMapping(value = REQUEST_PATH, method = RequestMethod.GET)
-    public String getFlights(final Model model) throws AirPortDaoException {
-        List<Flight> flights = flightService.getAll();
-        model.addAttribute("flights",flights);
-
-        return "flight/flights";
     }
 }
